75e241b21e296caadd789a1b26a379b88a48f440
Abhishek Rastogi <abhishek_rastogi@softprodigy.com>
2022-11-09 16:59:16 +0530

    Pushing code

---
+++ app/helpers/weather_helper.rb
@@ -0,0 +1,23 @@
+module WeatherHelper
+  def get_color
+    if @data.dig('weather').nil?
+        render :error_message
+    else
+        @data = @data
+    end
+
+    if @data.dig('weather').nil?
+      @api_color = "gray"
+    elsif @data['main']['temp'] < 0 
+      @api_color = "aquamarine"
+    elsif @data['main']['temp'] > 1 && @data['main']['temp'] < 10
+      @api_color = "white"
+    elsif @data['main']['temp'] > 11 && @data['main']['temp'] < 20
+      @api_color = "skyblue"
+    elsif @data['main']['temp'] > 21 && @data['main']['temp'] < 40
+      @api_color = "orange"
+    else
+      @api_color = "red"
+    end
+  end
+end
